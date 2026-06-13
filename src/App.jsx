441458f38851b5c5973de2c4372ab4ca18ef1946
SaraCuellar89 <saracuellar213@gmail.com>
2026-06-12 21:21:49 -0500

    despliegue - ver 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Inicio from "./paginas/Inicio";
+import Inicio from "./Paginas/Inicio";
 import Informacion from "./Paginas/Informacion";
 
 const App = () => {
